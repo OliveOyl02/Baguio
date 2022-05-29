8eb0b1538163324d29e4cd913f9deb5fcd291d9c
Dezza Antoinette Zoleta <dezzazoleta101@gmail.com>
2022-05-30 03:43:41 +0800

    added content

--- index.php
+++ index.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/skeleton.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Home</title>
 </head>
 <body>
+    <div class="container u-full-width u-max-full-width">
+        <div class="row full">
+            <div class="six columns welcome">
+                <h1>Welcome to Baguio City</h1>
+                <p>
+                    Nicknamed the summer capital of the Philippines thanks to its comparatively cool temperature. 
+                    Baguio City is one of the most popular places to stay on the island of Luzon. 
+                    From strawberry picking to craft beer to Mines View Park, here are the top things to do and experience <a href="">there</a>.</p>
+            </div>
+        </div>
+    </div>
+
+    <?php
     
+    ?>
 </body>
 </html>
