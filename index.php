62e288ac5e1ff6e03a71305d6f6137268d54f6d8
Dezza Antoinette Zoleta <dezzazoleta101@gmail.com>
2022-05-30 05:55:29 +0800

    fixed the index.php design

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
     <link rel="stylesheet" href="./css/normalize.css">
     <link rel="stylesheet" href="./css/skeleton.css">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Home</title>
+    <title>Baguio | Home</title>
 </head>
 <body>
-    <div class="container u-full-width u-max-full-width">
+    <div class="container contain u-full-width u-max-full-width">
         <div class="row full">
             <div class="six columns welcome">
                 <h1>Welcome to Baguio City</h1>
                 <p>
                     Nicknamed the summer capital of the Philippines thanks to its comparatively cool temperature. 
                     Baguio City is one of the most popular places to stay on the island of Luzon. 
-                    From strawberry picking to craft beer to Mines View Park, here are the top things to do and experience <a href="">there</a>.</p>
+                    From strawberry picking to craft beer to Mines View Park, here are the top things to do and experience <a href="./landmark.php">there</a>.</p>
             </div>
         </div>
     </div>
