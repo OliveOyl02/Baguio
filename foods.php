f4864b57436be2dff0f3d9ab1f7e28bc83e4a90c
Dezza Antoinette Zoleta <dezzazoleta101@gmail.com>
2022-05-30 07:08:41 +0800

    fixed heading and background

--- foods.php
+++ foods.php
@@ -33,8 +33,8 @@
         </div>
         <div class="row">
             <div class="twelve columns" style="background-color: black;">
-                <img src="./images/lmbg.webp" alt="" id="lmbg">
-                <h2 style="text-align: center; color:crimson;">Landmarks</h2>
+                <img src="./images/foodbg.jpg" alt="" id="lmbg">
+                <h2 style="text-align: center; color:crimson;">Foods and Sweets</h2>
             </div>
         </div>
         <?php
